07222e78339b655e637f10ef2c1b3d9d721ef555
tux <tuxwhitetiger@gmail.com>
2022-07-14 21:16:10 +0100

    .

--- CHIP/Program.cs
+++ CHIP/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using rpi_rgb_led_matrix_sharp;
+using System.Net.Sockets;
 using System.Net;
+using System.Text;
 
 namespace Speech.Recognition.Example
 {
@@ -10,6 +12,8 @@ namespace Speech.Recognition.Example
         {
             var matrix = new RGBLedMatrix(32, 2, 1);
             var canvas = matrix.CreateOffscreenCanvas();
+            String HOST = "127.0.0.1"; // The server's hostname or IP address
+            int PORT = 65432;  // The port used by the server
 
             for (var i = 0; i < 1000; ++i)
             {
@@ -25,18 +29,46 @@ namespace Speech.Recognition.Example
                 canvas.DrawLine(canvas.Width / 2 - 3, canvas.Height / 2 + 3, canvas.Width / 2 + 3, canvas.Height / 2 - 3, new Color(0, 0, 255));
 
                 canvas = matrix.SwapOnVsync(canvas);
-                var uriBuilder = new UriBuilder();
-                uriBuilder.Scheme = "http";
-                uriBuilder.Host = "webcode.me";
-                uriBuilder.Path = "/";
 
-                Uri uri = uriBuilder.Uri;
 
-                WebRequest request = WebRequest.Create(uri);
-                WebResponse response = request.GetResponse();
 
-                var headers = response.Headers;
-                Console.WriteLine(headers);
+
+                IPHostEntry hostEntry = Dns.GetHostEntry(HOST);
+
+                var ipe = new IPEndPoint(hostEntry.AddressList[0], PORT);
+                using var socket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(ipe);
+
+                if (socket.Connected)
+                {
+                    Console.WriteLine("Connection established");
+                }
+                else
+                {
+                    Console.WriteLine("Connection failed");
+                    return;
+                }
+
+                Byte[] requestBytes = Encoding.ASCII.GetBytes("Get Face");
+                Byte[] bytesReceived = new Byte[256];
+
+                socket.Send(requestBytes, requestBytes.Length, 0);
+
+                int bytes = 0;
+                var sb = new StringBuilder();
+
+                do
+                {
+                    bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
+                    sb.Append(Encoding.ASCII.GetString(bytesReceived, 0, bytes));
+                } while (bytes > 0);
+
+                Console.WriteLine(sb.ToString());
+
+
+
             }
         }
     }
