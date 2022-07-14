f6e78a8caae42c498261c556a3f24b05b5195825
tux <tuxwhitetiger@gmail.com>
2022-07-14 21:33:06 +0100

    .

--- CHIP/Program.cs
+++ CHIP/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using rpi_rgb_led_matrix_sharp;
 
-
 namespace CHIP
 {
     class MainClass
@@ -33,5 +32,50 @@ namespace CHIP
 
 
         }
+    }
+
+    class mynetwork
+    {
+        String HOST = "localhost"; // The server's hostname or IP address
+        int PORT = 65432;  // The port used by the server
+        Socket socket;
+
+        public mynetwork()
+        {
+
+
+        }
+
+        public void connect()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(HOST);
+            IPEndPoint ipe = new IPEndPoint(hostEntry.AddressList[1], PORT);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipe);
+            if (socket.Connected)
+            {
+                Console.WriteLine("Connection established");
+            }
+            else
+            {
+                Console.WriteLine("Connection failed");
+                return;
+            }
+        }
+
+        public String getFace()
+        {
+            Byte[] requestBytes = Encoding.ASCII.GetBytes("Get Face");
+            Byte[] bytesReceived = new Byte[256];
+            socket.Send(requestBytes, requestBytes.Length, 0);
+            int bytes = 0;
+            StringBuilder sb = new StringBuilder();
+            bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
+            sb.Append(Encoding.ASCII.GetString(bytesReceived, 0, bytes));
+            return sb.ToString();
+        }
+
+
+
     }
 }
