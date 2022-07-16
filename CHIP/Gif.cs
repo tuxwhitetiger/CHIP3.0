e2f68eefab2608fee3bc4e92b31d0fa892a065dc
tux <tuxwhitetiger@gmail.com>
2022-07-16 01:15:58 +0100

    .

--- CHIP/Gif.cs
+++ CHIP/Gif.cs
@@ -34,39 +34,41 @@ namespace CHIP
                 string[] pixels = frame.Split(',');
                 Console.WriteLine("pixel count:" + pixels.Length);
                 if (pixels.Length > 1)
+                {//pritty sure that we at the end of the file here
+                    break;
+                }
+                foreach (String Pixel in pixels)
                 {
-                    foreach (String Pixel in pixels)
+                    //split into color
+                    String trimmed = Pixel.Trim(charsToTrim);
+                    //Console.WriteLine(trimmed);
+                    string[] colors = trimmed.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    //if there more then 3 colors in pixel let me know
+                    if (colors.Length != 3)
                     {
-                        //split into color
-                        String trimmed = Pixel.Trim(charsToTrim);
-                        //Console.WriteLine(trimmed);
-                        string[] colors = trimmed.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                        //if there more then 3 colors in pixel let me know
-                        if (colors.Length != 3)
-                        {
-                            Console.WriteLine("we done fucked up with colors");
-                        }
-                        //put the color data into the array
-                        //this bit might change to make it easyer to push to display
-                        data[xcounter, ycounter, framecounter, 0] = Int32.Parse(colors[0]);
-                        data[xcounter, ycounter, framecounter, 1] = Int32.Parse(colors[1]);
-                        data[xcounter, ycounter, framecounter, 2] = Int32.Parse(colors[2]);
-                        //increment x counter
-                        xcounter++;
-                        //if we are at the end of the row go to start of next one
-                        if (xcounter == x)
-                        {
-                            xcounter = 0;
-                            //Console.WriteLine("next row");
-                            ycounter++;
-                        }
-                        //if there are more rows then expected let me know
-                        if (ycounter == y)
-                        {
-                            Console.WriteLine("we done fucked up with row counting");
-                        }
+                        Console.WriteLine("we done fucked up with colors");
+                    }
+                    //put the color data into the array
+                    //this bit might change to make it easyer to push to display
+                    data[xcounter, ycounter, framecounter, 0] = Int32.Parse(colors[0]);
+                    data[xcounter, ycounter, framecounter, 1] = Int32.Parse(colors[1]);
+                    data[xcounter, ycounter, framecounter, 2] = Int32.Parse(colors[2]);
+                    //increment x counter
+                    xcounter++;
+                    //if we are at the end of the row go to start of next one
+                    if (xcounter == x)
+                    {
+                        xcounter = 0;
+                        //Console.WriteLine("next row");
+                        ycounter++;
+                    }
+                    //if there are more rows then expected let me know
+                    if (ycounter == y)
+                    {
+                        Console.WriteLine("we done fucked up with row counting");
                     }
                 }
+                
                 //move to the next frame
                 framecounter++;
                 //if there are more frames then expected let me know
