9e4083c3dd3fe8ac252503f9f676d56108b56ef9
tux <tuxwhitetiger@gmail.com>
2022-07-16 14:41:28 +0100

    .

--- CHIP/Gif.cs
+++ CHIP/Gif.cs
@@ -1,5 +1,6 @@
 ﻿using rpi_rgb_led_matrix_sharp;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -23,62 +24,35 @@ namespace CHIP
         }
 
         public void loadData(String rawdata) {
+            gifstruct newgif = new gifstruct();
             //trim fat from the end of the message ,FRAMEDONE
             string[] fixedData = rawdata.Split("DONE");
             //split out the indevidual frames
-            string[] frames = fixedData[0].Split(",FRAME");
-            char[] charsToTrim = {'[',']'};
+            string[] frames = fixedData[0].Split("FRAME");
+            char[] charsToTrim = {'[',']',','};
+            
             foreach (String frame in frames) {
+                newgif.newframe();
                 Console.WriteLine("processing frame:" + newFrameCount);
-                int xcounter = 0;
-                int ycounter = 0;
-                //split to pixels
-                string[] pixels = frame.Split(',');
-                Console.WriteLine("pixel count:" + pixels.Length);
-                if (pixels.Length <= 1)
-                {//pritty sure that we at the end of the file here
-                    break;
-                }
-                foreach (String Pixel in pixels)
+                string[] rows = frame.Split("ROW");
+                foreach (String row in rows)
                 {
-                    //split into color
-                    String trimmed = Pixel.Trim(charsToTrim);
-                    //Console.WriteLine(trimmed);
-                    string[] colors = trimmed.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                    //if there more then 3 colors in pixel let me know
-                    if (colors.Length != 3)
-                    {
-                        Console.WriteLine("we done fucked up with colors");
+                    newgif.newrow();
+                    string[] pixels = frame.Split(',');
+                    if (pixels.Length <= 1)
+                    {//pritty sure that we at the end of the file here
+                        break;
                     }
-                    //put the color data into the array
-                    //this bit might change to make it easyer to push to display
-                    data[xcounter, ycounter, newFrameCount, 0] = Int32.Parse(colors[0]);
-                    data[xcounter, ycounter, newFrameCount, 1] = Int32.Parse(colors[1]);
-                    data[xcounter, ycounter, newFrameCount, 2] = Int32.Parse(colors[2]);
-                    //increment x counter
-                    xcounter++;
-                    //if we are at the end of the row go to start of next one
-                    if (xcounter == x)
+                    foreach (String Pixel in pixels)
                     {
-                        xcounter = 0;
-                        //Console.WriteLine("next row");
-                        ycounter++;
+                        String trimmed = Pixel.Trim(charsToTrim);
+                        string[] colors = trimmed.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        newgif.pumpData(Int32.Parse(colors[0]), Int32.Parse(colors[1]), Int32.Parse(colors[2]));
                     }
-                    //if there are more rows then expected let me know
-                    if (ycounter > y)
-                    {
-                        Console.WriteLine("we done fucked up with row counting");
-                    }
-                }
-
-                //move to the next frame
-                newFrameCount++;
-                //if there are more frames then expected let me know
-                if (newFrameCount == framecount)
-                {
-                    Console.WriteLine("we done fucked up with frame count");
                 }
             }
+            framecount = newgif.frames.Count;
+            data = newgif.toArray();
         }
 
         internal void printFrame(RGBLedMatrix matrix, RGBLedCanvas canvas,int myFrame)
---
+++ CHIP/gifstruct.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CHIP
+{
+    class gifstruct
+    {
+        public List<frame> frames = new List<frame>();
+        public gifstruct() {
+            
+        }
+        public void newframe()
+        {
+            frames.Add(new frame());
+        }
+        public void newrow()
+        {
+            frames[frames.Count - 1].rows.Add(new row());
+        }
+
+        public void pumpData(int r, int g, int b) {
+            int framecount = frames.Count -1;
+            int rowcount = frames[framecount].rows.Count -1;
+            frames[framecount].rows[rowcount].pixels.Add(new pixel());
+            int pixelcount = frames[framecount].rows[rowcount].pixels.Count -1;
+            frames[framecount].rows[rowcount].pixels[pixelcount].colors.Add(r);
+            frames[framecount].rows[rowcount].pixels[pixelcount].colors.Add(g);
+            frames[framecount].rows[rowcount].pixels[pixelcount].colors.Add(b);
+        }
+
+        public int[,,,] toArray()
+        {
+            //build corrrect size array
+            //x(cols), y(rows), framecount, color(0=r,1=g,2=b)
+            int rows = frames[frames.Count - 1].rows.Count;
+            int cols = frames[frames.Count - 1].rows[rows - 1].pixels.Count;
+            int framecount = frames.Count;
+            int[,,,] output = new int[cols,rows,framecount,3];
+            //shove the data into it
+            int x=0, y=0, z =0;
+            foreach (frame f in frames) {
+                foreach (row r in f.rows) {
+                    foreach (pixel p in r.pixels) {
+                        output[x, y, z, 0] = p.colors[0];
+                        output[x, y, z, 1] = p.colors[1];
+                        output[x, y, z, 2] = p.colors[2];
+                        x++;
+                    }
+                    y++;
+                }
+                z++;
+            }
+
+            return output;
+        }
+    }
+    public class frame
+    {
+        public List<row> rows = new List<row>();
+        public void newrow() {
+            rows.Add(new row());
+        }
+    }
+    public class row
+    {
+        public List<pixel> pixels = new List<pixel>();
+        public void newpixel()
+        {
+            pixels.Add(new pixel());
+        }
+    }
+    public class pixel
+    {
+        public List<int> colors = new List<int>();
+    }
+    
+}
