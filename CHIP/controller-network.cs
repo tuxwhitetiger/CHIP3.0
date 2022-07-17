aba2c425ab34ad63c077058da4c8ea59a72aa354
tux <tuxwhitetiger@gmail.com>
2022-07-17 22:33:02 +0100

    .

--- CHIP/controller-network.cs
+++ CHIP/controller-network.cs
@@ -14,21 +14,15 @@ namespace CHIP
         /// accsepts a controller assigns a port and waits for another to reconnect
         /// </summary>
         /// 
-        String HOST = "localhost"; // The server's hostname or IP address
-        int PORT = 65433;  // The port used by the server
-        int addressNumber = 1;
         Socket socket;
-        int nextPORT = 65434;
         public List<Controller> controllers = new List<Controller>();
         int controllercount = 1;
 
         public controller_network() {
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            IPAddress ipAddress = IPAddress.Parse("10.1.1.1");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 65433);
             socket.Bind(localEndPoint);
-            socket.Listen(100);
             Task.Factory.StartNew(checkForNewController, TaskCreationOptions.LongRunning);
         }
         public void checkForNewController() {
