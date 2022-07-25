0c8b23e1837be56d18c140fca879919766e66b91
tux <tuxwhitetiger@gmail.com>
2022-07-25 20:36:18 +0100

    .

--- CHIP/magic8Ball.cs
+++ CHIP/magic8Ball.cs
@@ -53,6 +53,7 @@ namespace CHIP
 
         public void drawFace(RGBLedMatrix matrix, RGBLedCanvas canvas) {
             //calculate and position on face or design and preset?
+            canvas.Clear();
             canvas.DrawText(font, 7, 23, new Color(255, 255, 255), predictions[currentPrediction]);
             canvas = matrix.SwapOnVsync(canvas);
         }
