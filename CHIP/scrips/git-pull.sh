4c22c8b2d6adb7ab7f76d6e7aa0c668dc63b2e1c
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-10-08 18:46:41 +0100

    git

---
+++ CHIP/scrips/git-pull.sh
@@ -0,0 +1,6 @@
+﻿if ping -c 1 1.1.1.1 &> /dev/null
+then
+  git -C /home/tux/CHIP pull https://github.com/tuxwhitetiger/CHIP.git
+  git -C /home/tux/CHIP3 pull https://github.com/tuxwhitetiger/CHIP3.0.git
+else
+  echo "no internet"
