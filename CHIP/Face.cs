3fa369a23b38867ccb76a265d0837be7d9c1ee40
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-10-08 23:12:58 +0100

    ..

--- CHIP/Face.cs
+++ CHIP/Face.cs
@@ -64,7 +64,7 @@ namespace CHIP
             options.GpioSlowdown = 3;
             options.HardwareMapping = "regular";
             options.ScanMode = 0;
-            options.Multiplexing= 2;//TEST LINE!!!
+            options.Multiplexing= 3;//TEST LINE!!!
             mylogger.Log("matrix pop");
             matrix = new RGBLedMatrix(options);
             mylogger.Log("canvas pop");
