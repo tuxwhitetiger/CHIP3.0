8a4921e80407971d5a42d3795e4ec229bbcdc7e8
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-10-11 20:47:12 +0100

    ..

--- CHIP/Face.cs
+++ CHIP/Face.cs
@@ -332,7 +332,7 @@ namespace CHIP
                 dae.height = 16;
                 faceAnimationDelay = 33;
 
-                HSVS.SetSpeed(3);
+                HSVS.SetSpeed(30);
 
                 dae.setup= true;
             }
