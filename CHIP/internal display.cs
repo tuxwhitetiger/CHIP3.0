4155804b7b4bd63da86b120dea70a6a5055d9d50
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-10-15 01:01:01 +0100

    ..

--- CHIP/internal display.cs
+++ CHIP/internal display.cs
@@ -5,7 +5,6 @@ using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Drawing;
 using SixLabors.ImageSharp.Processing.Text;
 using SixLabors.Primitives;
-using Ssd130;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -72,7 +71,7 @@ namespace CHIP
             using (SSD130 Ssd130 = new SSD130())
             {
                 mylogger.Log("Ssd130 Begin");
-                Ssd130.Begin(1);
+                Ssd130.Begin(mylogger,1);
                 mylogger.Log("Ssd130 Clear");
                 Ssd130.Clear();
                 mylogger.Log("Ssd130 Display");
