f172fb9b61c2d9b0fa6027bc737ec80db5a5f8f3
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-10-15 19:53:56 +0100

    ..

--- CHIP/textSpam.cs
+++ CHIP/textSpam.cs
@@ -12,13 +12,46 @@ namespace CHIP
         int counter = 0;
         Stopwatch timer = new Stopwatch();
         Random rand = new Random();
+        tpart[] tparts;
+        int tpartscount = 0;
+        int poition = 0;
+
         internal void start(RGBLedCanvas canvas, RGBLedMatrix matrix) {
             timer.Restart();
             canvas.Clear();
             matrix.SwapOnVsync(canvas);
         }
-
         internal void Tick(string text, int count, int time, RGBLedCanvas canvas, RGBLedMatrix matrix, RGBLedFont font)
+        { 
+            //check if this new command
+            if(tpartscount!=count)
+            {
+                tpartscount = count;
+                tparts = new tpart[tpartscount];
+                poition = 0;
+                canvas.Clear();
+            }
+            if (poition == tpartscount-1) {
+                poition = 0;
+            }
+            tparts[poition] = generatetpart();
+            poition++;
+
+            foreach(tpart part in tparts)
+            {
+                canvas.DrawText(font, part.x, part.y, part.col, text);
+            }
+        }
+
+        private tpart generatetpart() {
+            tpart t = new tpart();
+            t.x = rand.Next(0, 128);
+            t.y = rand.Next(0, 32);
+            t.col = new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+            return t;
+        }
+
+        internal void oldTick(string text, int count, int time, RGBLedCanvas canvas, RGBLedMatrix matrix, RGBLedFont font)
         {
             if (startted) {
                 if (timer.ElapsedMilliseconds > 2000) {
@@ -52,5 +85,11 @@ namespace CHIP
         }
 
         
+    }
+
+    struct tpart {
+        public int x;
+        public int y;
+        public Color col;
     }
 }
