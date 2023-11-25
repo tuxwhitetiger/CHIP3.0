eb85fff04a16de8ac5b7327617a9d53763700e55
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-11-25 20:14:57 +0000

    .

--- CHIP/internal display.cs
+++ CHIP/internal display.cs
@@ -20,7 +20,7 @@ namespace CHIP
     {
         Image<Rgba32> image = new Image<Rgba32>(128, 32);
         SSD130 Ssd130 = new SSD130();
-        Font font = SystemFonts.CreateFont("Noto Mono", 25, FontStyle.Regular);
+        Font font = SystemFonts.CreateFont("Noto Mono", 16, FontStyle.Regular);
         Logger mylogger;
 
         public internal_display(Logger mylogger) {
@@ -38,7 +38,7 @@ namespace CHIP
         public void PrintTime() {
             var time = DateTime.Now;
             mylogger.Log("Mutate");
-            image.Mutate(ctx => ctx.Fill(Rgba32.Black).DrawText($"{time.Hour.ToString("D2")}:{time.Minute.ToString("D2")}:{time.Second.ToString("D2")}", font, Rgba32.White, new PointF(0, 0)));
+            image.Mutate(ctx => ctx.Fill(Rgba32.Black).DrawText($"{time.Hour.ToString("D2")}:{time.Minute.ToString("D2")}:{time.Second.ToString("D2")}", font, Rgba32.White, new PointF(16, 0)));
             mylogger.Log("Ssd130 Image");
             Ssd130.Image(image);
             mylogger.Log("Ssd130 Display");
@@ -53,7 +53,7 @@ namespace CHIP
 
         internal void update(string face)
         {
-            
+            PrintTime();
             printText(face);
         }
     }
