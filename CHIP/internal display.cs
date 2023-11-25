a52bdd62cf1fd028f51622b20731cf19d73728f3
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-11-25 20:17:04 +0000

    .

--- CHIP/internal display.cs
+++ CHIP/internal display.cs
@@ -38,7 +38,7 @@ namespace CHIP
         public void PrintTime() {
             var time = DateTime.Now;
             mylogger.Log("Mutate");
-            image.Mutate(ctx => ctx.Fill(Rgba32.Black).DrawText($"{time.Hour.ToString("D2")}:{time.Minute.ToString("D2")}:{time.Second.ToString("D2")}", font, Rgba32.White, new PointF(16, 0)));
+            image.Mutate(ctx => ctx.Fill(Rgba32.Black).DrawText($"{time.Hour.ToString("D2")}:{time.Minute.ToString("D2")}:{time.Second.ToString("D2")}", font, Rgba32.White, new PointF(0, 16)));
             mylogger.Log("Ssd130 Image");
             Ssd130.Image(image);
             mylogger.Log("Ssd130 Display");
@@ -46,7 +46,8 @@ namespace CHIP
         }
 
         public void printText(String text) {
-            image.Mutate(ctx => ctx.Fill(Rgba32.Black).DrawText(text, font, Rgba32.White, new PointF(0, 0)));
+            var time = DateTime.Now;
+            image.Mutate(ctx => ctx.DrawText(text, font, Rgba32.White, new PointF(0, 0)));
             Ssd130.Image(image);
             Ssd130.Display();
         }
