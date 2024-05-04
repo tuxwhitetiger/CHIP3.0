8de8067803cd3ea6d3a3fb5867d520b3591e35ed
tuxwhitetiger <tuxwhitetiger@gmail.com>
2024-05-04 18:49:17 +0100

    ..

--- CHIP/WebServer.cs
+++ CHIP/WebServer.cs
@@ -229,10 +229,12 @@ public static string pageData =
                     int row = 0;
                     int col = 0;
                     Color[,] colorData = new Color[64,32];
-                    mylogger.Log("s1 length : " + colours[0].Length);
+                    mylogger.Log("s1 length : " + colours[0].Length + " " + colours[0]);
                     foreach (string s1 in colours)
                     {
+                        mylogger.Log("row : " + row + " col:"+col);
                         Color c = new Color(Convert.ToInt32(s1.Substring(0,2), 16), Convert.ToInt32(s1.Substring(2, 2), 16), Convert.ToInt32(s1.Substring(4,2), 16));
+                        mylogger.Log("c : " + c.ToString());
                         colorData[row,col] = c;
                         row++;
                         if(row == 63) {
--- CHIP/obj/Debug/netcoreapp3.1/CHIP.AssemblyInfo.cs
+++ CHIP/obj/Debug/netcoreapp3.1/CHIP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CHIP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd895cd4dcb8b9fe34571d80de13732bfb07dc5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36f3c393339d7e1d05085c5e962b883dddc5966")]
 [assembly: System.Reflection.AssemblyProductAttribute("CHIP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CHIP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
